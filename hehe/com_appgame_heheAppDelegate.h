81c62db68f84195fb1e61256f83c84827f17d179
计 炜 <goooajw@gmail.com>
2014-01-11 22:46:18 +0800

    Initial Commit

---
+++ hehe/com_appgame_heheAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_appgame_heheAppDelegate.h
+//  hehe
+//
+//  Created by 计 炜 on 14-1-11.
+//  Copyright (c) 2014年 计 炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_appgame_heheAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hehe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hehe
+//
+//  Created by 计 炜 on 14-1-11.
+//  Copyright (c) 2014年 计 炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_appgame_heheAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_appgame_heheAppDelegate class]));
+    }
+}
